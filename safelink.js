6391001b3860f39815a3bd9499f00588a9f88446
krowe15 <33015056+krowe15@users.noreply.github.com>
2017-10-23 12:51:19 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2017/10/atelier-totori-adventurer-of-arland.html"}
 if(x=="2"){xxx="/2017/10/call-of-duty-wwii.html"}
 if(x=="3"){xxx="/2017/10/gt-sport.html"}
 if(x=="4"){xxx="/2017/10/horizon-zero-dawn.html"}
-if(x=="5"){xxx="/2017/10/tekken-7.html"}a_to_ve[i].href="http:///www.moneycome.me"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/10/tekken-7.html"}a_to_ve[i].href="http://www.moneycome.me"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
